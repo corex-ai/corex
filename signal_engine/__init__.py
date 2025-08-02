3f507fb96c6c04c1a53bb2e43c272717203a849a
corex-ai <corex.ai.system@gmail.com>
2025-08-02 23:25:15 +0530

    Delete signal_engine directory

--- signal_engine/__init__.py
+++
@@ -1,6 +0,0 @@
-from .indicators import calculate_indicators
-from .price_fetcher import fetch_price_data
-
-def generate_signal(price_data):
-    indicators = calculate_indicators(price_data)
-    return indicators
--- signal_engine/price_fetcher.py
+++
@@ -1,2 +0,0 @@
-def fetch_price_data():
-    return [100, 102, 101, 103, 104]
